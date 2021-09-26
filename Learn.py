c4be8a2ce27e01622cd6c12f6056084a545ad75c
Almer <m.abrarzakialmer@gmail.com>
2021-09-26 13:08:58 +0700

    First commit

---
+++ Learn.py
@@ -0,0 +1,46 @@
+# Variable
+
+a = 2 # integer
+hewan = "Gajah" # String
+b = 'A' # String
+c= True # Boolean
+d = False
+e= 2.0 # Float
+f = [] # List/Array
+g = {} # Dictionary
+h = () # Tuple
+
+print(hewan)
+
+# Print
+
+print(hewan)
+print("APA?")
+
+# Print 2
+
+print("Nama hewannya adalah: " + hewan)
+print("Nama hewannya adalah: {}".format(hewan))
+
+print("Nama apa aja deh: " + str(a))
+
+print("Nama hewannya adalah: " + str(a))
+print("Nama hewannya adalah: {}".format(a))
+
+# {}
+print("Nama: {}, TL: {}, Umur: {}".format("Almer", "Malang", 20))
+
+Nama_Siapa = ["Almer", "Zaki", "Abrar", "Alvin"]
+print(Nama_Siapa[0])
+print(Nama_Siapa[1])
+print(Nama_Siapa[2])
+
+List_1 = ["Alvin"] * 4
+List_1
+Nama_Siapa[0:2] #angka pertama itu mulai, angka terakhir itu berakhir sebelum angka itu
+
+Nama_Siapa[-1] #ngambil di belakang list/array
+Nama_Siapa[-2:] 
+Nama_Siapa.append(2) #input data ke var array
+print(Nama_Siapa)
+Nama_Siapa[0:3]
---
+++ TestWithKaAlvin.py
@@ -0,0 +1,2 @@
+Nama_Siapa = ["Almer", "Zaki", "Abrar", "Alvin"]
+Nama_Siapa[0:3]
